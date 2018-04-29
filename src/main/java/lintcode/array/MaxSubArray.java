623243e9b62383a685ac93bd87eadb4654d2aba5
leonzmq520 <leonzmq520@gmail.com>
2018-04-29 22:52:39 +0200

    Add MaxSubArray with comments

--- src/main/java/lintcode/array/MaxSubArray.java
+++ src/main/java/lintcode/array/MaxSubArray.java
@@ -1,5 +1,36 @@
 package lintcode.array;
 
+/**
+ * 41. Maximum Subarray
+ * 
+ * Difficulty:Easy
+ * 
+ * Accepted Rate: 38%
+ * 
+ * Given an array of integers, find a contiguous subarray which has the largest
+ * sum.
+ *
+ *
+ *
+ * Example:
+ * 
+ * Given the array [−2,2,−3,4,−1,2,1,−5,3], the contiguous subarray [4,−1,2,1]
+ * has the largest sum = 6.
+ * 
+ * 
+ * 
+ * Challenge: Can you do it in time complexity O(n)?
+ * 
+ * 
+ * 
+ * Tags: LinkedIn Subarray Greedy LintCode Copyright Microsoft Bloomberg Array
+ * Enumeration
+ * 
+ * 
+ * 
+ * @author fakezmq
+ *
+ */
 public class MaxSubArray {
 	/**
 	 * @param nums:
